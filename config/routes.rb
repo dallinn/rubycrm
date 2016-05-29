789acb0fcbf7314b659d738d202c4e112e4db6a5
dallin <dallin@localhost.localdomain>
2016-05-29 12:50:29 -0600

    Update sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#show'
 
   resources :posts
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
