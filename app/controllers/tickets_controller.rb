af31d46c60df47d1ff017bd854405368e4417196
dallinn <dallinn@users.noreply.github.com>
2016-05-30 16:17:55 -0600

    Update ticket views

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -69,6 +69,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.fetch(:ticket, {})
+      params.require(:ticket).permit(:title, :company, :body)
     end
 end
